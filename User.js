5918b43a6eb6b2efb0d50190589145d161f1abfa
skinc <shane.skikne@students.olin.edu>
2014-03-22 16:14:55 -0400

    sending to greg

--- User.js
+++ User.js
@@ -4,11 +4,11 @@ var user_schema = mongoose.Schema({
 	name: String,
 	email: String,
 	locationString: String,
-	location : [Number],
+	location: [Number],
 	SMS: Boolean,
 	html:Boolean
 });
 
-var user = mongoose.model('songcard', user_schema);
+var user = mongoose.model('user', user_schema);
 
 module.exports = user;
